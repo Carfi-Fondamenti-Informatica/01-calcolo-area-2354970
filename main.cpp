d6ac850069fc615fe539f154fb32bf83d674a33d
2354970 <143269060+2354970@users.noreply.github.com>
2023-08-28 23:09:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main() {
-    float a=0, b=0, c=0, triangle_area=0, square_area=0, rectangle_area=0, trapeze_area=0;
+    float a=0, b=0, c=0,
+    
     cin >> a >> b >> c;
-    triangle_area=(a*b)/2;
-    square_area=a*a;
-    rectangle_area=a*b;
-    trapeze_area=((a+b)*c)/2;
-    cout << triangle_area << square_area << rectangle_area << trapeze_area << endl;
+    
+    cout << (a*b)/2 << endl;
+    cout << a*a << endl;
+    cout << a*b << endl;
+    cout << ((a+b)*c)/2 << endl;
     return 0;
 }
