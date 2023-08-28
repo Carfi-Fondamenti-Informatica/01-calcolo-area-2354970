5267e6656ffcceea4e6bfe2a78ce79bcbc210793
2354970 <143269060+2354970@users.noreply.github.com>
2023-08-28 22:54:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
-float a=0, b=0, c=0, triangle_area=0, square_area=0, rectangle_area=0, trapeze_area=0;
-cin >> a >> b >> c;
-//Qui va scritto il codice per eseguire i calcoli richiesti
-triangle_area=(a*b)/2;
-square_area=a*a;
-rectangle_area=a*b;
-trapeze_area=((a+b)*c)/2;
-cout << triangle_area << " " << square_area << " " << rectangle_area << " " << trapeze_area << endl;
-return 0;
+int main() {
+    float a=0, b=0, c=0, triangle_area=0, square_area=0, rectangle_area=0, trapeze_area=0;
+    cin >> a >> b >> c;
+    triangle_area=(a*b)/2;
+    square_area=a*a;
+    rectangle_area=a*b;
+    trapeze_area=((a+b)*c)/2;
+    cout << triangle_area << " " << square_area << " " << rectangle_area << " " << trapeze_area << endl;
+    return 0;
 }
