f2bac84a716815d4f899af6c07e05b4cdfad462b
2354970 <143269060+2354970@users.noreply.github.com>
2023-08-28 22:49:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main(){
 //Qui vanno definite le variabili
 float a=0, b=0, c=0, triangle_area=0, square_area=0, rectangle_area=0, trapeze_area=0;
 cin >> a >> b >> c;
-
 //Qui va scritto il codice per eseguire i calcoli richiesti
 triangle_area=(a*b)/2;
 square_area=a*a;
